394a6ffb17ddef7cd9645c7ff7207cc1c2c0efcf
Lucas Bastos <lucsbasto@gmail.com>
2024-09-12 12:22:06 -0300

    feat: add motion to homepage

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,9 @@
 "use client"
+import { motion } from "framer-motion"
 import Image from "next/image"
 import Link from "next/link"
 import { useState } from "react"
 import NavLink from "./NavLink"
-
 const links = [
   {url: '/', title: 'Home'},
   {url: '/about', title: 'About'},
@@ -13,6 +13,58 @@ const links = [
 
 export function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+  const middleVariants = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    },
+  };
+
+  const bottomVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+
+  
+  const listVariants = {
+    closed: {
+      x: "100vw",
+    },
+    opened: {
+      x: 0,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const listItemVariants = {
+    closed: {
+      x: -10,
+      opacity: 0,
+    },
+    opened: {
+      x: 0,
+      opacity: 1,
+    },
+  };
 
   return (
     <div className="h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48 text-xl">
@@ -55,25 +107,42 @@ export function Navbar() {
       {/* RESPONSIVE MENU */}
       <div className="md:hidden">
           {/* MENU BUTTON */}
-          <button className="w-10 h-8 flex flex-col justify-between z-50 relative" 
-          onClick={() => setIsOpen(!isOpen)}>
-            <div className="w-10 h-1 bg-black rounded"></div>
-            <div className="w-10 h-1 bg-black rounded"></div>
-            <div className="w-10 h-1 bg-black rounded"></div>
+          <button      className="w-10 h-8 flex flex-col justify-between z-50 relative"
+          onClick={() => setIsOpen((prev) => !prev)}>
+            <motion.div
+            variants={topVariants}
+            animate={isOpen ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
+          ></motion.div>
+          <motion.div
+            variants={middleVariants}
+            animate={isOpen ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded"
+          ></motion.div>
+          <motion.div
+            variants={bottomVariants}
+            animate={isOpen ? "opened" : "closed"}
+            className="w-10 h-1 bg-black rounded origin-left"
+          ></motion.div>
           </button>
           {/* MENU LIST */}
           {
             isOpen && (
-              <div className="absolute top-0 left-0 w-screen h-screen text-white flex flex-col items-center justify-center gap-8 text-4xl">
+              <motion.div
+              variants={listVariants}
+              initial="closed"
+              animate="opened"
+              className="absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col items-center justify-center gap-8 text-4xl z-40">
               {links.map(link => (
-                <Link 
-                  href={link.url} 
-                  key={link.url} 
-                  className="font-semibold text-black mr-4">
-                  {link.title}
-                </Link>
+                <motion.div key={link.url} variants={listItemVariants}>
+                  <Link 
+                    href={link.url} 
+                    className="font-semibold text-white mr-4">
+                    {link.title}
+                  </Link>
+                </motion.div>
               ))}
-          </div>
+          </motion.div>
             )
           }
          
